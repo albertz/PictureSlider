9c2be618a4a673187a8008a90045e82c7517652a
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-08 15:04:11 +0100

    Initial Commit

---
+++ PictureSlider/PictureSliderView.h
@@ -0,0 +1,13 @@
+//
+//  PictureSliderView.h
+//  PictureSlider
+//
+//  Created by Albert Zeyer on 08.12.11.
+//  Copyright (c) 2011 Albert Zeyer. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PictureSliderView : ScreenSaverView
+
+@end
---
+++ PictureSlider/PictureSliderView.m
@@ -0,0 +1,52 @@
+//
+//  PictureSliderView.m
+//  PictureSlider
+//
+//  Created by Albert Zeyer on 08.12.11.
+//  Copyright (c) 2011 Albert Zeyer. All rights reserved.
+//
+
+#import "PictureSliderView.h"
+
+@implementation PictureSliderView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
