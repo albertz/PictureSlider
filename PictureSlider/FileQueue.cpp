6329d6bc2be6d3f44ca1920f230759e9ae716f41
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-08 22:43:05 +0100

    filter only (good) pictures

--- PictureSlider/FileQueue.cpp
+++ PictureSlider/FileQueue.cpp
@@ -16,6 +16,7 @@
 #include <assert.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 std::mt19937_64 rnd;
 __attribute__((constructor)) static void _rand_engine__init() {
@@ -24,6 +25,22 @@ __attribute__((constructor)) static void _rand_engine__init() {
 
 static uint64_t rndInt(uint64_t min, uint64_t max) {
 	return std::uniform_int_distribution<uint64_t>(min,max)(rnd);
+}
+
+static bool _isGoodFile(const std::string& basefn, struct dirent* dp) {
+	auto l = dp->d_namlen;
+	if(l <= 4) return false;
+	if(dp->d_name[l-4] == '.' &&
+	   (dp->d_name[l-3] == 'j' || dp->d_name[l-3] == 'J') &&
+	   (dp->d_name[l-2] == 'p' || dp->d_name[l-2] == 'P') &&
+	   (dp->d_name[l-1] == 'g' || dp->d_name[l-1] == 'G')) {
+		std::string fn = basefn + "/" + dp->d_name;
+		struct stat s;
+		if(stat(fn.c_str(), &s) != 0) return false;
+		if(s.st_size < 1*1024*1024) return false; // pictures of size <1MB are too small
+		return true;
+	}
+	return false;
 }
 
 static const float C_nonloaded_dirs_expectedFac = 0.5;
@@ -50,9 +67,10 @@ struct Dir {
 			if(strcmp(dp->d_name, "iPhoto Library") == 0) continue;
 			if(strcmp(dp->d_name, "iPod Photo Cache") == 0) continue;
 			if(strcmp(dp->d_name, "Photo Booth") == 0) continue;
-			if(dp->d_type == DT_REG)
-				files.push_back(dp->d_name);
-			else if(dp->d_type == DT_DIR) {
+			if(dp->d_type == DT_REG) {
+				if(_isGoodFile(base, dp))
+					files.push_back(dp->d_name);
+			} else if(dp->d_type == DT_DIR) {
 				std::shared_ptr<Dir> subdir(new Dir);
 				subdir->base = base + "/" + dp->d_name;
 				nonloadedDirs.push_back(subdir);
