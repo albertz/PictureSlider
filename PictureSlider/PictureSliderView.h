368f07997c86301cf4d099db402b08f565904288
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-09 13:57:22 +0100

    picture slider: first working code

--- PictureSlider/PictureSliderView.h
+++ PictureSlider/PictureSliderView.h
@@ -7,7 +7,15 @@
 //
 
 #import <ScreenSaver/ScreenSaver.h>
+#import <QuartzCore/CAAnimation.h>
+#import <QuartzCore/CoreImage.h>
 
 @interface PictureSliderView : ScreenSaverView
+{
+	NSImageView     *currentImageView;          // an NSImageView that displays the current image, as a subview of the SlideshowView
+	CIImage         *inputShadingImage;         // an environment-map image that the transition filter may use in generating the transition effect
+	CIImage         *inputMaskImage;            // a mask image that the transition filter may use in generating the transition effect
 
+	NSTimer* slideshowTimer;
+}
 @end
