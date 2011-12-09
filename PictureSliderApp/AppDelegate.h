45aba959a1990e91f71cc2b5e0c259101b40f509
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-09 17:47:40 +0100

    some simple app

---
+++ PictureSliderApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PictureSliderApp
+//
+//  Created by Albert Zeyer on 09.12.11.
+//  Copyright (c) 2011 Albert Zeyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSWindowDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ PictureSliderApp/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  PictureSliderApp
+//
+//  Created by Albert Zeyer on 09.12.11.
+//  Copyright (c) 2011 Albert Zeyer. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "PictureSliderView.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{	
+	NSLog(@"applicationDidFinishLaunching");
+	[[self window] setContentView:[[PictureSliderView alloc] init]];
+}
+
+@end
