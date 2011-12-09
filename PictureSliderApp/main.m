50e6a80c5e19d1ba53e02bcb9be6baebe3cfc03f
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2011-12-09 17:48:11 +0100

    more on PictureSliderApp

---
+++ PictureSliderApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PictureSliderApp
+//
+//  Created by Albert Zeyer on 09.12.11.
+//  Copyright (c) 2011 Albert Zeyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	return NSApplicationMain(argc, (const char **)argv);
+}
